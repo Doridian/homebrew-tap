45005d3af9b571a685427e1b2d43808c0e6012c4
Doridian <git@doridian.net>
2021-11-19 08:15:53 -0800

    New version

--- Formula/displayplacer.rb
+++ Formula/displayplacer.rb
@@ -1,8 +1,8 @@
 class Displayplacer < Formula
   desc "macOS command line utility to configure multi-display resolutions and arrangements. Essentially XRandR for macOS."
   homepage "https://github.com/doridian/displayplacer"
-  url "https://github.com/Doridian/displayplacer/archive/refs/tags/2.0.0.tar.gz"
-  sha256 "724be22af7830ecf14bd724a22853738b3834fbc2fb4a8953c383ea06dc5a01e"
+  url "https://github.com/Doridian/displayplacer/archive/refs/tags/2.1.0.tar.gz"
+  sha256 "f5093f196cb2c0c5530e2e454838cec6de428ba626ed70cd4868baad0cab52e2"
 
   def install
     system "make"
